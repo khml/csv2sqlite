8f5949913f0802ead1bcc7a9a40f09acf2c74fa6
khml <43922054+khml@users.noreply.github.com>
2023-09-10 19:04:56 +0900

    [add] InsertRecords func

--- src/libc2s/repository.go
+++ src/libc2s/repository.go
@@ -1,6 +1,9 @@
 package libc2s
 
-import "database/sql"
+import (
+	"database/sql"
+	"io"
+)
 
 func ConnectRepository(filepath string) (*Repository, error) {
 	db, err := ConnectDatabase(filepath)
@@ -27,4 +30,40 @@ func (r Repository) CreateTbl(tblName string, csv *CsvData) error {
 
 func (r Repository) Exec(query string) (sql.Result, error) {
 	return r.db.Exec(query)
+}
+
+func (r Repository) InsertRecords(tableName string, data *CsvData) (int, error) {
+	// counter for record num
+	numRecords := 0
+
+	// build query
+	query := buildInsertRecordQuery(tableName, data.HeaderRow)
+
+	// read records, one line at a time.
+	for {
+		record, err := data.Reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return numRecords, err
+		}
+
+		// insert record
+		// set params
+		args := make([]interface{}, len(record))
+		for i, v := range record {
+			args[i] = v
+		}
+
+		// exec insert record SQL
+		_, err = r.db.Exec(query, args...)
+		if err != nil {
+			return numRecords, err
+		}
+
+		numRecords++
+	}
+
+	return numRecords, nil
 }
