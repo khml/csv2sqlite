82653be0dbc77be294d24b24db5f9f155bc5f849
khml <43922054+khml@users.noreply.github.com>
2023-09-10 18:56:21 +0900

    [modify] sort repository def

--- src/libc2s/repository.go
+++ src/libc2s/repository.go
@@ -15,10 +15,10 @@ type Repository struct {
 	db *sql.DB
 }
 
-func (r Repository) Exec(query string) (sql.Result, error) {
-	return r.db.Exec(query)
-}
-
 func (r Repository) Close() error {
 	return r.db.Close()
+}
+
+func (r Repository) Exec(query string) (sql.Result, error) {
+	return r.db.Exec(query)
 }
